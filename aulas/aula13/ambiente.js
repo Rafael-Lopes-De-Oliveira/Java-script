61d9568fa76e92337f13058cf7c9de978dd234bb
Rafael-Lopes-De-Oliveira <157087018+Rafael-Lopes-De-Oliveira@users.noreply.github.com>
2025-05-28 20:39:49 -0300

    nada em comum so testes

---
+++ aulas/aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+
+while( c <= 5){
+    console.log(`tudo bem ${c}`)
+    c++
+}
+
+
+
+
+
+
+
+
+
+// console.log('hello world')
+// console.log('hello world')
+// console.log('hello world')
+// console.log('hello world')
+// console.log('hello world')
+// console.log('hello world')
+// console.log('hello world')
+// console.log('hello world')
