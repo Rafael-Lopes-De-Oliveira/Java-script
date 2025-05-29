2551cb38f1ebeb20d6f05ff75e8f0742ce241afc
Rafael-Lopes-De-Oliveira <157087018+Rafael-Lopes-De-Oliveira@users.noreply.github.com>
2025-05-28 22:28:24 -0300

    teste o do while

--- aulas/aula13/ambiente.js
+++ aulas/aula13/ambiente.js
@@ -1,18 +1,32 @@
 // var c = 1
 
-var pizza = ''
-var  quant = 16
-var acomer = 1
+// var pizza = ''
+// var  quant = 16
+// var acomer = 1
 
-if(pizza == 'margerita' || pizza == ''){
-    console.log('PIZZA RUIM, JOGAR FORA')
-}else if (pizza != 'margerita' ){
-    while (acomer <= quant) {
-        console.log(`comi a ${acomer} fatia de ${pizza}`)
-        acomer++
+// if(pizza == 'margerita' || pizza == ''){
+//     console.log('PIZZA RUIM, JOGAR FORA')
+// }else if (pizza != 'margerita' ){
+//     while (acomer <= quant) {
+//         console.log(`comi a ${acomer} fatia de ${pizza}`)
+//         acomer++
 
-    }
+//     }
+// }
+
+var podeapont = 3
+var vaiapont = 4
+
+if(vaiapont >= podeapont){
+ console.log('Putz man, F lápis')
+ console.log(`O lapis podia ser apontado ${podeapont} vezes, e você apontou ${vaiapont} vezes`)
+}else{
+    do{
+        console.log(` O lápis foi apontado ${vaiapont} de  ${podeapont} apontadas`)
+        vaiapont++
+    }while(vaiapont <= podeapont)
 }
+
 
 // while( c <= 5){
 //     console.log(`tudo bem ${c}`)
