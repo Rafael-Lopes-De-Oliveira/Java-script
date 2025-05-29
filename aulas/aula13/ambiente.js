6331c99993b732068a3acdb36469de4fd9d1b8bb
Rafael-Lopes-De-Oliveira <157087018+Rafael-Lopes-De-Oliveira@users.noreply.github.com>
2025-05-28 21:12:44 -0300

    repetições
    aprendendo a utilizar o While, e o Do While

--- aulas/aula13/ambiente.js
+++ aulas/aula13/ambiente.js
@@ -1,9 +1,23 @@
-var c = 1
+// var c = 1
 
-while( c <= 5){
-    console.log(`tudo bem ${c}`)
-    c++
+var pizza = ''
+var  quant = 16
+var acomer = 1
+
+if(pizza == 'margerita' || pizza == ''){
+    console.log('PIZZA RUIM, JOGAR FORA')
+}else if (pizza != 'margerita' ){
+    while (acomer <= quant) {
+        console.log(`comi a ${acomer} fatia de ${pizza}`)
+        acomer++
+
+    }
 }
+
+// while( c <= 5){
+//     console.log(`tudo bem ${c}`)
+//     c++
+// }
 
 
 
