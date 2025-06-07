8b093803f9d2f3db9700a09f82abe146b8f60b97
Rafael-Lopes-De-Oliveira <157087018+Rafael-Lopes-De-Oliveira@users.noreply.github.com>
2025-06-07 15:14:50 -0300

    Tabuada feita
    Utilizando repetição, o FOR

---
+++ aulas/aula14ex/ex017/script.js
@@ -0,0 +1,24 @@
+function tabu(){
+    var numb = window.document.querySelector('#numero')
+ 
+    var numb= Number(numb.value)
+
+    var msg = window.document.querySelector('#msg')
+
+    if(numb == ''){
+        window.alert(`Campo numerico esta vazio, valor alterado para 0`)
+    }
+
+    var result = 0
+
+    for(var cont = 0; cont <= 10; cont += 1 ){
+        var  result = numb * cont
+
+        msg.innerHTML += ` <br> ${numb} X ${cont} = ${result} `
+    }
+
+}
+
+function carregar(){
+    body.style = ''
+}
