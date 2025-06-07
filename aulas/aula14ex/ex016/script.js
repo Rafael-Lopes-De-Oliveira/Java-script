3c0e7832fc3af167740d5d7651c4c0345507afb5
Rafael-Lopes-De-Oliveira <157087018+Rafael-Lopes-De-Oliveira@users.noreply.github.com>
2025-06-07 07:33:35 -0300

    Exercicio feito

--- aulas/aula14ex/ex016/script.js
+++ aulas/aula14ex/ex016/script.js
@@ -16,16 +16,18 @@ function contar(){
     var pass = Number(pass.value)
 
 
-    if( ini == 0 || end == 0){
+    if( end == 0){
         window.alert('valor nao existente!')
-    }else if( pass == 0){
+    }else if( pass == 0 || pass == ''){
         window.alert(`valor de passos é ${pass} mudando seu valor para 1 `)
+        pass = 1
+    }else if(ini < 0){
+        window.alert('impossivel de proseguir no momento' )
     }
 
-    // msg.innerHTML = `O INICIO: ${ini} , O FIM: ${end} e PASSOS: ${pass}`
 
     for(var cont = ini; cont <= end; cont += pass ){
-        msg.innerHTML = `${cont}`
+        msg.innerHTML += `  [${cont}] `
     }
 
 
