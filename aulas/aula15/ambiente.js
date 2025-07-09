a5b33e72739eb1e327bc3695a3a5161aa57b68f3
Rafael-Lopes-De-Oliveira <157087018+Rafael-Lopes-De-Oliveira@users.noreply.github.com>
2025-07-09 09:42:03 -0300

    Arrays
    Fazendo Arrays

---
+++ aulas/aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [8,6,9,0,5]
+
+num.push(2,5);
+console.log(num);
+console.log(`Esse é o primeiro número:${num[0]} e esse é o ultimo:${num[6] }`)
+
+num.sort();
+console.log(num);
+console.log(`Esse é o primeiro número:${num[0]} e esse é o ultimo:${num[6] }`)
---
+++ testes/teste04/nada.js
@@ -0,0 +1,6 @@
+let teste = [5,3,6,4]
+
+teste.push(4,5,99)
+teste.sort()
+
+console.log(`Olha que numero belo ${teste}`)
