9ed07f4887f2fa3a555d3d1ea8e2433d53ac787a
Rafael-Lopes-De-Oliveira <157087018+Rafael-Lopes-De-Oliveira@users.noreply.github.com>
2025-07-23 00:01:56 -0300

    projeto funções
    criando minha primeira calculadora

---
+++ mini_projetos/calculadora/calculadora.js
@@ -0,0 +1,23 @@
+function somar(a ,b){
+    const soma = a + b;
+    return soma;
+}
+
+
+function vezes(a ,b){
+    const veze = a * b;
+    return veze;
+}
+
+function dividir(a , b){
+    const dividi = a / b;
+    return dividi
+}
+
+
+function menos(a, b){
+    const meno = a - b;
+    return meno
+}
+
+
