3ebcc4a14bd25b5b602538be874a29d5349ce445
Rafael-Lopes-De-Oliveira <157087018+Rafael-Lopes-De-Oliveira@users.noreply.github.com>
2025-07-23 23:27:51 -0300

    calculadora

--- mini_projetos/calculadora/calculadora.js
+++ mini_projetos/calculadora/calculadora.js
@@ -1,23 +1,17 @@
-function somar(a ,b){
-    const soma = a + b;
-    return soma;
+
+const nume = (num) =>{
+  window.document.querySelector('#olhador').innerHTML += num;
 }
 
-
-function vezes(a ,b){
-    const veze = a * b;
-    return veze;
+const limpe = () =>{
+  return window.document.querySelector('#olhador').innerHTML = ""
 }
 
-function dividir(a , b){
-    const dividi = a / b;
-    return dividi
-}
-
-
-function menos(a, b){
-    const meno = a - b;
-    return meno
+const resultado = () =>{
+  let result = window.document.querySelector('#olhador').innerHTML;
+  if(result){  
+    result = window.document.querySelector('#olhador').innerHTML = eval(result)
+  }
 }
 
 
