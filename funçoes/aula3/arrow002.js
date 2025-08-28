42db116ceb521334e5123c81907dc4cf5e8d027c
Rafael-Lopes-De-Oliveira <157087018+Rafael-Lopes-De-Oliveira@users.noreply.github.com>
2025-08-28 20:11:51 -0300

    nothing

---
+++ funçoes/aula3/arrow002.js
@@ -0,0 +1,17 @@
+const people = (name,age,sex) => {
+    return{
+        name,
+        age,
+        sex,
+        most (){
+            return ` Name: ${name} age: ${age} sex:${sex}`
+        }
+    }
+};
+
+let userRafael = people('Rafael','17','Masculino')
+
+let userf = people(false,'15',false);
+let userAges = userRafael.age + userf.age
+console.log(userAges);
+// console.log(userRafael.most());
