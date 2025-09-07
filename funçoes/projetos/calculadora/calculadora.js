11716025b9ade378299a222908fdaaa9c8847b03
Rafael-Lopes-De-Oliveira <157087018+Rafael-Lopes-De-Oliveira@users.noreply.github.com>
2025-09-07 12:41:48 -0300

    better

--- funçoes/projetos/calculadora/calculadora.js
+++ funçoes/projetos/calculadora/calculadora.js
@@ -1,21 +1,32 @@
 
 let result = window.document.querySelector('#olhador');
+let g = 0;
+const limpe = () => result.innerHTML = "";
 
 const nume = (num) =>{
   result.innerHTML += num;
-  
+  result.value += num;
 }
-
-
-const limpe = () =>{
-  return result.innerHTML = ""
-}
+let valguardado = 0
 
 const resultado = () =>{
-  let math = result.innerHTML
-  result.innerHTML= ''
-  return result.innerHTML = eval(math)
+  let math = result.value;
+  result.innerHTML = eval(math);
 }
+
+let Mmais = window.document.querySelector('#Mmais').addEventListener('click', function(){
+  result.innerHTML = "";
+  valguardado = result.value;
+  console.log(valguardado);
+ } )
+
+let Mmenos = window.document.querySelector('#Mmenos').addEventListener('click', function(){
+  result.innerHTML = valguardado;
+  console.log(valguardado);
+} )
+
+const  point = (pt) => result.innerHTML += pt;
+
 
 
 
