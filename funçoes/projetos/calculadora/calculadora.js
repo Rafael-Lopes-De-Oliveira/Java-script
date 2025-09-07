d846049549692ba6065dbc1a94942824d49e4702
Rafael-Lopes-De-Oliveira <157087018+Rafael-Lopes-De-Oliveira@users.noreply.github.com>
2025-09-07 18:22:46 -0300

    melhorando a sintaxe

--- funçoes/projetos/calculadora/calculadora.js
+++ funçoes/projetos/calculadora/calculadora.js
@@ -1,32 +1,36 @@
 
 let result = window.document.querySelector('#olhador');
-let g = 0;
 const limpe = () => result.innerHTML = "";
+let valguardado = true;
 
 const nume = (num) =>{
   result.innerHTML += num;
   result.value += num;
+  
 }
-let valguardado = 0
+
+
+const ponto = window.document.querySelector('#ponto').addEventListener('click', () => result.innerHTML += '.')
+
+// let Mmais = window.document.querySelector('#Mmais').addEventListener('click', function(){
+//   valguardado = result.value;
+//   result.innerHTML = "";
+// } )
+
+
+// Mmais.addEventListener('dblclick', function(){
+//   valguardado = 0;
+// });
+
+// let Mmenos = window.document.querySelector('#Mmenos').addEventListener('click', function(){
+//   result.innerHTML = ""
+//   result.innerHTML = valguardado;
+// } )
+
 
 const resultado = () =>{
-  let math = result.value;
-  result.innerHTML = eval(math);
+  let math =  eval(result.value);
+  result.innerHTML = math;
 }
-
-let Mmais = window.document.querySelector('#Mmais').addEventListener('click', function(){
-  result.innerHTML = "";
-  valguardado = result.value;
-  console.log(valguardado);
- } )
-
-let Mmenos = window.document.querySelector('#Mmenos').addEventListener('click', function(){
-  result.innerHTML = valguardado;
-  console.log(valguardado);
-} )
-
-const  point = (pt) => result.innerHTML += pt;
-
-
 
 
